4c66d68c8375e901b9cd3be0875908fc1521f788
Aditya Singh <asingh7@thelcfgroup.com>
2025-07-02 12:07:54 -0400

    Changes to be committed: 	deleted:    hello.py 	modified:   main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Hello, World!')
+print('Calculator')
+
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+
+choice = input('Enter operation (+ or -): ')
+
+def add(x, y):
+    z = x + y
+    return z
+
+def subtract(x, y):
+    z = x - y
+    return z
+
+
+if choice == '+':
+    result = add(x, y)
+    z = result
+    print(z)
+
+elif choice == '-':
+    result = subtract(x, y)
+    z = result
+    print(z)
+
+
